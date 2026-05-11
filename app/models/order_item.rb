97f40d56893cdb86ea1a9bf209d69d130e175715
Shahbazh1 <shahbazhashmi235@gmail.com>
2026-05-11 15:43:30 +0500

    clean the models

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,7 +4,3 @@ class OrderItem < ApplicationRecord
   belongs_to :product_variant
 end
 
-# OrderItem Model Missing Associations:
-# Migration: order_items table has product_id and product_variant_id with foreign keys.
-# Model: Only belongs_to :order defined.
-# Issue: Missing belongs_to :product and belongs_to :product_variant in OrderItem model.
